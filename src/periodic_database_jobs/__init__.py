b48852afd1677dce24772618b565b69b8e9ed8da
Gerard Weatherby <gweatherby@uchc.edu>
2025-12-19 15:30:46 -0500

    add dependencies, fix import, implement

--- src/periodic_database_jobs/__init__.py
+++ src/periodic_database_jobs/__init__.py
@@ -1,7 +1,8 @@
 
 import importlib.metadata 
 import logging
-periodic_database_jobs_logger = logging.getLogger(__name__)
+periodic_db_logger = logging.getLogger(__name__)
 
 __version__ =  importlib.metadata.version('periodic_database_jobs') 
 
+from periodic_database_jobs.scheduled import run_jobs
--- src/periodic_database_jobs/main.py
+++ src/periodic_database_jobs/main.py
@@ -1,16 +1,27 @@
 #!/usr/bin/env python3
 import argparse
 import logging
-from periodic_database_jobs import periodic_database_jobs_logger
+
+import yaml
+from postgresql_access import DatabaseDict
+
+from periodic_database_jobs import periodic_db_logger, run_jobs
 
 
 def main():
     logging.basicConfig()
     parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
     parser.add_argument('-l', '--loglevel', default='WARN', help="Python logging level")
+    parser.add_argument('yaml',help='Configuration')
 
     args = parser.parse_args()
-    periodic_database_jobs_logger.setLevel(getattr(logging,args.loglevel))
+    periodic_db_logger.setLevel(getattr(logging, args.loglevel))
+    with open(args.yaml) as f:
+        config = yaml.safe_load(f)
+    db = DatabaseDict(dictionary=config['database'])
+    with db.connect(application_name="periodic db jobs") as conn:
+        run_jobs(config,conn)
+
 
 
 if __name__ == "__main__":
